3081a93b76a8ab6b7ccab197159fa5444d36541c
Raphael Cerqueira <rcerqueira@vr.com.br>
2021-03-18 18:22:26 -0300

    Initial Commit

---
+++ MercadoLivre/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MercadoLivre
+//
+//  Created by Raphael Cerqueira on 18/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MercadoLivre/MercadoLivreApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercadoLivreApp.swift
+//  MercadoLivre
+//
+//  Created by Raphael Cerqueira on 18/03/21.
+//
+
+import SwiftUI
+
+@main
+struct MercadoLivreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
